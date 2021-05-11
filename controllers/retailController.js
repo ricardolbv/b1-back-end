ca36c29faf7b3c1814d8cfd987b22fc0136ec5d2
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-05-11 18:56:54 -0300

    Implementando o metodo POST para varejo

--- controllers/retailController.js
+++ controllers/retailController.js
@@ -1,4 +1,4 @@
-const { QueryTypes } = require('sequelize');
+const sequelize = require('sequelize');
 const db = require('../database/db');
 
 const selectAll = async () => {
@@ -10,6 +10,26 @@ const selectAll = async () => {
     }
 }
 
+const insert = async (retailDTO) => {
+    try {
+        const sql = `INSERT INTO varejo
+        (inscricao, cnpj, razao_social, nome_fantasia, telefone, status, id_cargo, id_login, id_segmento, created_at, updated_at)
+        VALUES('${retailDTO.inscricao}','${retailDTO.cnpj}','${retailDTO.razao_social}','${retailDTO.nome_fantasia}','${retailDTO.telefone}',
+                ${retailDTO.status},'${retailDTO.id_cargo}', '${retailDTO.id_login}', '${retailDTO.id_segmento}',
+                '${retailDTO.created_at}', '${retailDTO.updated_at}')`;
+
+        const retail = await db.query(sql, {
+            type: db.QueryTypes.INSERT
+        }); 
+        console.log(retail) 
+
+        return { data: 'Sucesso ao inserir novo varejo'}
+    } catch (error) {
+        return { error: error.message }
+    }
+}
+
 module.exports = {
     selectAll,
+    insert,
 }
--- routes/retail.js
+++ routes/retail.js
@@ -19,11 +19,64 @@ router.get("/", async (req, res, next) => {
   const data = await retailController.selectAll();
   if (data.error) {
     res.statusCode = 500;
-    res.send({ status: "error", data: data.error });
+    return res.send({ status: "error", data: data.error });
   }
 
   res.statusCode = 200;
-  res.send({ status: "ok", data: data });
+  return res.send({ status: "ok", data: data });
+});
+
+/**
+ * @swagger
+ * /retail:
+ *  post:
+ *    summary: Cria um varejo 
+ *    parameters:
+ *      - in: body
+ *        name: Varejo
+ *        schema:
+ *          type: object
+ *          properties:
+ *            inscricao:
+ *              type: string
+ *            cnpj:
+ *              type: string
+ *            razao_social:
+ *              type: string
+ *            nome_fantasia:
+ *              type: string
+ *            telefone:
+ *              type: string 
+ *            status:
+ *              type: integer
+ *            id_cargo:
+ *              type: integer
+ *            id_login:
+ *              type: integer
+ *            id_segmento:
+ *              type: integer
+ *            created_at:
+ *              type: string
+ *            updated_at:
+ *              type: string
+ *    responses:
+ *      '200':
+ *        description:  Varejo criado com sucesso
+ *      '400':
+ *        description: Erro ao criar varejo
+ *      '500':
+ *        description: Erro ao criar varejo
+ *    tags:
+ *      - varejo
+ */
+ router.post("/", async (req, res, next) => {
+  const data = await retailController.insert(req.body)
+  if (data.error) {
+    res.statusCode = 500;
+    return res.send({ status: "error", data: data.error });
+  }
+  res.statusCode = 200;
+  return res.send({ status: "ok", data: data.data });
 });
 
 module.exports = router;
