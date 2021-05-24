274e28e1a9eeac001ce8035259231737a3e98aaf
Pedro <pedro.ms1@puccampinas.edu.br>
2021-05-24 20:32:39 -0300

    brand

--- routes/brands.js
+++ routes/brands.js
@@ -19,7 +19,56 @@ router.get("/", function (req, res, next) {
   const data = brandController.selectAll();
   if (data.error) {
     res.statusCode = 500;
-    res.send({ status: "error", data: "Erro ao buscar usuarios" });
+    res.send({ status: "error", data: "Erro ao buscar marcas" });
+  }
+
+  res.statusCode = 200;
+  res.send({ status: "ok", data: data });
+});
+
+/**
+ * @swagger
+ * /brands/create:
+ *  post:
+ *    summary: Cadastra uma marca
+ *    parameters:
+ *      - in: body
+ *        name: Marca
+ *        schema:
+ *          type: object
+ *          properties:
+ *            email:
+ *              type: string
+ *            senha:
+ *              type: string
+ *            nome:
+ *              type: string
+ *            cnpj:
+ *              type: string
+ *            telefone:
+ *              type: string
+ *            id_cargo:
+ *              type: integer
+ *            id_segmento:
+ *              type: integer
+ *            id_varejo:
+ *              type: integer
+ *    responses:
+ *      '200':
+ *        description:  Marca cadastrado com sucesso
+ *      '400':
+ *        description: Email informado já está sendo utilizado
+ *      '500':
+ *        description: Erro ao cadastrar marca
+ *    tags:
+ *      - marca
+ */
+
+router.post("/create", function (req, res, next) {
+  const data = brandController.createBrand(req.body);
+  if (data.error) {
+    res.statusCode = 500;
+    res.send({ status: "error", data: "Erro ao criar marca" });
   }
 
   res.statusCode = 200;
