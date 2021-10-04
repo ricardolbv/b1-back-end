1c740d2ba7b6de3fd44cc99a637997cd7792815c
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-10-04 20:12:22 -0300

    Adicionando erros ao logar

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,31 +3,30 @@ const jwt = require("jsonwebtoken");
 const secret = "b1";
 
 const verifyLogin = async (req) => {
-  var email = req.email;
-  var senha = req.senha;
-  var retorno = await db.query(
-    `select * from dbo.login where email = '${email}'`
-  );
-  var status = {};
-  var data = JSON.parse(JSON.stringify(retorno[0]));
-  if (!data[0]) {
-    status.status = 0;
-    return res.json(status);
-  } else if (data[0].senha != senha) {
-    status.status = 1;
-    return res.json(status);
-  } else {
-    var token = jwt.sign(
-      {
-        usuarioId: data[0].id,
-        cargoId: data[0].id_cargo,
-        email: data[0].email,
-      },
-      secret,
-      { expiresIn: 1000 }
-    );
-    return token;
-  }
+  try{
+      var email = req.email;
+      var senha = req.senha;
+      var retorno = await db.query(
+        `select * from dbo.login where email = '${email}'`
+      );
+      var data = JSON.parse(JSON.stringify(retorno[0]));
+
+      if (!data[0] || data[0].senha != senha)
+        return {error: 'Usuário inexistente ou senha inválida'};
+
+      var token = jwt.sign({
+            usuarioId: data[0].id,
+            cargoId: data[0].id_cargo,
+            email: data[0].email,
+          },
+          secret,
+          { expiresIn: 1000 }
+        );
+        return token;
+
+    } catch(err){
+      return {error: 'Usuário inexistente ou senha inválida'};
+    }
 };
 
 module.exports = {
